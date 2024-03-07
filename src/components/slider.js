13687a2c00debc44b8ffc13c90cf6c4360df0412
Sera Summer <148694494+SaeedRsera9388@users.noreply.github.com>
2024-03-07 10:54:58 +0400

    Add files via upload

---
+++ src/components/slider.js
@@ -0,0 +1,70 @@
+// Product img 
+import Product1 from '../assets/Product1.jpg'
+import Product2 from '../assets/Product2.jpg'
+import Product3 from '../assets/Product3.jpg'
+import Product4 from '../assets/Product4.jpg'
+import Product5 from '../assets/Product5.jpg'
+import Product6 from '../assets/Product6.jpg'
+import Product7 from '../assets/Product7.jpg'
+import Product8 from '../assets/Product8.jpg'
+import Product9 from '../assets/Product9.jpg'
+import Product10 from '../assets/Product10.jpg'
+
+// Brand img 
+
+import Brand1 from '../assets/Brand1.png'
+import Brand2 from '../assets/Brand2.png'
+import Brand3 from '../assets/Brand3.png'
+import Brand4 from '../assets/Brand1.png'
+import Brand5 from '../assets/Brand4.png'
+import Brand6 from '../assets/Brand2.png'
+import Brand7 from '../assets/Brand6.png'
+import Brand8 from '../assets/Brand5.png'
+import Brand9 from '../assets/Brand4.png'
+import Brand10 from '../assets/Brand5.png'
+
+
+const sliderData = [
+    {
+            image: Product1,
+            brand: Brand1,
+    },
+    {
+            image: Product2,
+            brand: Brand2,
+    },
+    {
+            image: Product3,
+            brand: Brand3,
+    },
+    {
+            image: Product4,
+            brand: Brand4,
+    },
+    {
+            image: Product5,
+            brand: Brand5,
+    },
+    {
+            image: Product6,
+            brand: Brand6,
+    },
+    {
+            image: Product7,
+            brand: Brand7,
+    },
+    {
+            image: Product8,
+            brand: Brand8,
+    },
+    {
+            image: Product9,
+            brand: Brand9,
+    },
+    {
+            image: Product10,
+            brand: Brand10,
+    },
+]
+
+export default sliderData
